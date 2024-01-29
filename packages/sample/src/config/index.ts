f1aa83cc24f2f66b1207ce73b1fbc1ad9889625a
Felix <chuanfa.chai@gmail.com>
2024-01-29 14:44:10 +0800

    feat: new chain

--- packages/sample/src/config/index.ts
+++ packages/sample/src/config/index.ts
@@ -1,7 +1,7 @@
 export const accountContracts = {
   BTC: [
     {
-      chainIds: [686868, 28206, 1102, 1502, 22776, 212, 80001],
+      chainIds: [686868, 28206, 1102, 1501, 1502, 22776, 212, 80001],
       version: '1.0.0',
     },
   ],
