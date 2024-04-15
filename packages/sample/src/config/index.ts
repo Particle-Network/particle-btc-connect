4e5778ad0274b3995bf8c3ec8bea5ff005081735
Leo Wang <wangwenlong@minijoy.com>
2024-04-15 11:01:34 +0800

    feat: update aa config

--- packages/sample/src/config/index.ts
+++ packages/sample/src/config/index.ts
@@ -1,20 +1,18 @@
 export const accountContracts = {
-  BTC: {
-    NAME: 'BTC Account',
-    SUPPORTED_CHAIN_IDS: [4200, 686868, 28206, 11503, 1501, 1502, 22776, 212, 3110, 1],
-    BATCH_TX: true,
-    VERSION: '1.0.0',
-  },
-  BTC_V2: {
-    NAME: 'BTC Account',
-    SUPPORTED_CHAIN_IDS: [200810, 3636, 2442, 1123, 5000, 5003, 2648, 111],
-    BATCH_TX: true,
-    VERSION: '2.0.0',
-  },
-  UNIVERSAL: {
-    NAME: 'Universal Account',
-    SUPPORTED_CHAIN_IDS: [11155111, 2011],
-    BATCH_TX: true,
-    VERSION: '1.0.0',
-  },
+  BTC: [
+    {
+      chainIds: [4200, 686868, 28206, 11503, 1501, 1502, 22776, 212, 3110, 1],
+      version: '1.0.0',
+    },
+    {
+      chainIds: [200810, 3636, 2442, 1123, 5000, 5003, 2648, 111],
+      version: '2.0.0',
+    },
+  ],
+  UNIVERSAL: [
+    {
+      chainIds: [11155111, 2011],
+      version: '1.0.0',
+    },
+  ],
 };
