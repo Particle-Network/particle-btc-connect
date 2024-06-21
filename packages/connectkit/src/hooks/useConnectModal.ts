3f527eaf37383a3637a63b4e51d85c8777e184f9
Felix <chuanfa.chai@gmail.com>
2024-06-21 15:34:36 +0800

    feat: new chains

--- packages/connectkit/src/hooks/useConnectModal.ts
+++ packages/connectkit/src/hooks/useConnectModal.ts
@@ -1,6 +1,7 @@
 import { useConnectProvider } from '../context';
 
 export const useConnectModal = () => {
+  //todo: return async connect status
   const { openConnectModal, disconnect } = useConnectProvider();
   return { openConnectModal, disconnect };
 };
