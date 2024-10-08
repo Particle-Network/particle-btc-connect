383f33ecd9835292def207c4d9dbdcf6cbdfea95
Felix <chuanfa.chai@gmail.com>
2024-10-08 10:23:48 +0800

    fix: config

--- packages/sample/src/config/index.ts
+++ packages/sample/src/config/index.ts
@@ -1,22 +1,38 @@
+import { chains } from '@particle-network/chains';
+
+export type BTCConfig = {
+  chainIds: number[];
+  version: string;
+};
+
 export const accountContracts = {
-  BTC: [
-    {
-      chainIds: [
-        4200, 686868, 223, 1123, 200901, 200810, 11501, 11503, 1501, 1502, 3109, 3110, 22776, 212, 60808, 111, 8329,
-        83291, 1116, 1115, 3636, 89682, 1, 11155111, 137, 80002, 2442, 56, 97, 42161, 421614, 5000, 5003, 2649, 2648,
-        34443, 919, 20,
-      ],
-      version: '1.0.0',
-    },
-    {
-      chainIds: [
-        4200, 686868, 223, 1123, 200901, 200810, 11501, 11503, 1501, 1502, 3109, 3110, 22776, 212, 60808, 111, 8329,
-        83291, 1116, 1115, 3636, 89682, 1, 11155111, 137, 80002, 2442, 56, 97, 42161, 421614, 5000, 5003, 2649, 2648,
-        34443, 919, 20,
-      ],
-      version: '2.0.0',
-    },
-  ],
+  BTC: chains
+    .getAllChainInfos()
+    .filter((chain) => chain.chainType === 'evm')
+    .filter(
+      (chain) =>
+        chain.features?.some(
+          (feature) => feature.name === 'ERC4337' && feature.contracts?.some((contract) => contract.name === 'BTC')
+        )
+    )
+    .reduce((value, current) => {
+      const versins = current.features
+        ?.find((feature) => feature.name === 'ERC4337')
+        ?.contracts?.filter((contract) => contract.name === 'BTC')
+        .map((contract) => contract.version) as string[];
+      versins.forEach((version) => {
+        const configItem = value.find((config) => config.version === version);
+        if (configItem) {
+          configItem.chainIds.push(current.id);
+        } else {
+          value.push({
+            version,
+            chainIds: [current.id],
+          });
+        }
+      });
+      return value;
+    }, [] as BTCConfig[]),
 };
 
 export type ContractName = keyof typeof accountContracts;
